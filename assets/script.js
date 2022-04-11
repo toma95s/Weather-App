31f2b993abb9432d6be3b6ab0dcba69d055123af
Asika Habib <asikahabib@gmail.com>
2022-04-11 16:29:06 -0700

    updated pseudo code added skeleton and updated html

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,32 @@
 //initialize the variables
 //implement the open weather api
 //Current Weather API key
-const apiKey = "58d56143837e18e5352b2e203d75f67f"
+const apiKey = "58d56143837e18e5352b2e203d75f67f";
 
 //jquery to dynamically append elements
+//create a variable dynamically through jquery
+var currentWeather = $("#currentWeather");
 
 
+//need an array to put in 
 //will need a function for search button
+//jquery will be good as a click once function
+$("#searchButton").click(function(){
+    event.preventDefault();
+    let citySearch = $("#citySearch").val();
+    //need to call the functions to return the value 
+    //of current weather and forecast
+    
+    
+})
+
+
+
 
 ////need a function to show city history
+function cityHistory(){
+
+}
 
 //need to use local storage to store the searches
 
