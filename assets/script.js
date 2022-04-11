2633e09fd07f4ba525146584702e1313fb36ae76
Asika Habib <asikahabib@gmail.com>
2022-04-10 23:26:24 -0700

    added pseudocode outline for my JS script

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+//initialize the variables
+//implement the open weather api
+//Current Weather API key
+const apiKey = "58d56143837e18e5352b2e203d75f67f"
+
+//jquery to dynamically append elements
+
+
+//will need a function for search button
+
+////need a function to show city history
+
+//need to use local storage to store the searches
+
+//need a function for the API
+
+//a function for the forecast
+
+//a function for the UV Index
+
+//need to get current weather if not in storage
+
+//will need an if/else statement for the colors
+
+
+//need another if statement if city clicked 
+
+
