61b5909c1f644ceebe2792cfc4c136368d78e1a2
Asika Habib <asikahabib@gmail.com>
2022-04-11 18:05:58 -0700

    dynamically creating elements on javascript

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,31 @@
+/* User Story
+
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+
+
+Acceptance Criteria
+
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+
+
+
+*/
+
+
 //initialize the variables
 //implement the open weather api
 //Current Weather API key
@@ -5,16 +33,21 @@ const apiKey = "58d56143837e18e5352b2e203d75f67f";
 
 //jquery to dynamically append elements
 //create a variable dynamically through jquery
-var currentWeather = $("#currentWeather");
+//var currentWeather = $("#currentWeather");
+
+//var currentDay = moment().format('L');
+//$("#currentDate").html(currentDay);
+//going to put the current day in the current City function
 
 
 //need an array to put in 
 //will need a function for search button
 //jquery will be good as a click once function
-$("#searchButton").click(function(){
+$("#searchButton").click(function(event){
     event.preventDefault();
     let citySearch = $("#citySearch").val();
     //need to call the functions to return the value 
+    currentCityWeather(citySearch);
     //of current weather and forecast
     
     
@@ -28,6 +61,35 @@ function cityHistory(){
 
 }
 
+function currentCityWeather(cities){
+    var currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=${cities}&appid=${apiKey}=imperial&units=imperial';
+
+    $.ajax({
+        URL: currentWeatherURL,
+        method: "GET"
+    }).then(function(result){
+        console.log(result);
+        console.log(currentWeatherURL);
+        //empty all of the city weather details
+        $("#cityDetails").empty();
+        //get the current date
+        var currentDate = moment().format('L');
+
+
+        //dynamically create the html elmenets
+
+        var currentCity =$("<h2>").text(result.name);
+        var newCurrentDate =$("<h6>").text(currentDate);
+
+        var temperature = $("<p>").text("Temperature: " + result.main.temp + "</p>");
+        var humidity = $("<p>").text("Humidity: " + result.main.humidity + "</p>");
+        var windSpeed = $("<p>").text("Wind Speed:" + result.main.wind.speed + "</p>");
+
+        var weatherIcon = result.weather[0].main ;
+
+
+    })
+}
 //need to use local storage to store the searches
 
 //need a function for the API
